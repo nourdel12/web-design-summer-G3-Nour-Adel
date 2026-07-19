ee43a7559a6c29abe4db8b3f8c17f43567ddd229
nourdel12 <nouradel122006@gmail.com>
2026-07-19 12:48:39 +0300

    Add initial HTML structure and JavaScript for grade evaluation

---
+++ WEEK3/TasksDay1/grades.js
@@ -0,0 +1,13 @@
+var grade=prompt("Enter your grade: ");
+if (grade >=90 && grade <100) {
+    console.log("A")
+}
+else if (grade >= 80 && grade < 90) {
+    console.log("B")
+}
+else if (grade >= 70 && grade < 80) {
+    console.log("C")
+}
+else {
+    console.log("F")
+}
